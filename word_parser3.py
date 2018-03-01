1b2a7ba88368bfcdd5adbf51bb624f14b256f79f
shorya <shoryakashyapsinha@gmail.com>
2018-03-01 13:21:39 +0530

    This produces both .txt as well as .docx file as output

--- word_parser3.py
+++ word_parser3.py
@@ -1,6 +1,6 @@
 from docx import Document
 from docx.shared import Inches
-
+import docx2txt
 
 def main():
     document_read = raw_input("Enter file name or path\n")
@@ -8,6 +8,7 @@ def main():
     num = 1
     for para in document.paragraphs:
         name = "Topic"+str(num -1)+".docx"
+        name1 = "Topic"+str(num - 1)+".txt"
         if para.style.name == 'Heading 1' and num == 1:
             doc = Document()
             sty = int(para.style.name[8])
@@ -15,6 +16,10 @@ def main():
             num = num + 1
         elif para.style.name == 'Heading 1' and num > 1:
             doc.save(name)
+            text = docx2txt.process(name)
+            f = open(name1, "w")
+            f.write(text)
+            f.close()
             doc = Document()
             doc.add_heading(para.text, 1)
             num = num + 1
@@ -24,4 +29,8 @@ def main():
         else:
             doc.add_paragraph(para.text)
     doc.save(name)
+    text1 = docx2txt.process(name)
+    f1 = open(name1, "w")
+    f1.write(text1)
+    f1.close()
 main()
